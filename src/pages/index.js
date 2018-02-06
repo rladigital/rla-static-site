7305a19c28a75ee5d13ede690e1be4d33be2df26
Tom Lawton <tom.lawton@rla.co.uk>
2018-02-06 23:14:53 +0000

    Updating the index

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default class IndexPage extends React.Component {
 
     render() {
         const { data } = this.props;
-        const { edges: people } = data.allMarkdownRemark;
+        //const { edges: people } = data.allMarkdownRemark;
 
         return (
             <section>
@@ -32,63 +32,27 @@ export default class IndexPage extends React.Component {
                         <h1>30 Years of Delivering</h1>
                         <h2>Strategic, Profitable Communications</h2>
                     </div>
-                    {people
-                        .filter(
-                            post => post.node.frontmatter.personKey === "person"
-                        )
-                        .map(({ node: post }) => (
-                            <div
-                                className="content"
-                                style={{
-                                    border: "1px solid #eaecee",
-                                    padding: "2em 4em"
-                                }}
-                                key={post.id}
-                            >
-                                <p>
-                                    <Link
-                                        className="has-text-primary"
-                                        to={post.frontmatter.path}
-                                    >
-                                        {post.frontmatter.title}
-                                    </Link>
-                                    <span> &bull; </span>
-                                    <small>{post.frontmatter.date}</small>
-                                </p>
-                                <p>
-                                    {post.excerpt}
-                                    <br />
-                                    <br />
-                                    <Link
-                                        className="button is-small"
-                                        to={post.frontmatter.path}
-                                    >
-                                        Keep Reading →
-                                    </Link>
-                                </p>
-                            </div>
-                        ))}
                 </div>
             </section>
         );
     }
 }
 
-export const pageQuery = graphql`
-    query IndexQuery {
-        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-            edges {
-                node {
-                    excerpt(pruneLength: 400)
-                    id
-                    frontmatter {
-                        title
-                        templateKey
-                        date(formatString: "MMMM DD, YYYY")
-                        path
-                    }
-                }
-            }
-        }
-    }
-`;
+// export const pageQuery = graphql`
+//     query IndexQuery {
+//         allMarkdownRemark {
+//             edges {
+//                 node {
+//                     excerpt(pruneLength: 400)
+//                     id
+//                     frontmatter {
+//                         title
+//                         templateKey
+//                         date(formatString: "MMMM DD, YYYY")
+//                         path
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// `;
