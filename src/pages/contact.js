cde6f792ed930e448f8781967a29bbde0e32a925
Tom Lawton <tom.lawton@rla.co.uk>
2018-02-16 17:06:43 +0000

    Working on adding Leaflet as the mapping library

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,17 +2,24 @@ import React from "react";
 import Link from "gatsby-link";
 import graphql from "graphql";
 import { Row, Column } from "rla-components";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
 import theme from "../theme/theme";
 import SolutionSummary from "../components/solutions/SolutionSummary";
 import HeaderBlock from "../components/HeaderBlock";
 
+require("leaflet/dist/leaflet.css");
+
 export default class SolutionsPage extends React.Component {
+    state = {
+        lat: 51.505,
+        lng: -0.09,
+        zoom: 13
+    };
     render() {
-        const {
-            data: { allMarkdownRemark: { edges: solutions } }
-        } = this.props;
+        const { data: { allMarkdownRemark: { edges: contacts } } } = this.props;
         //console.log(work);
+        const position = [this.state.lat, this.state.lng];
         return (
             <div>
                 <Row>
@@ -27,10 +34,33 @@ export default class SolutionsPage extends React.Component {
                 </Row>
 
                 <Row>
-                    {solutions.map(({ node: solution }, index) => {
+                    <Column>
+                        <Map
+                            center={position}
+                            zoom={this.state.zoom}
+                            style={{ height: "300px" }}
+                        >
+                            <TileLayer
+                                attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            <Marker position={position}>
+                                <Popup>
+                                    <span>
+                                        A pretty CSS3 popup. <br /> Easily
+                                        customizable.
+                                    </span>
+                                </Popup>
+                            </Marker>
+                        </Map>
+                    </Column>
+                </Row>
+
+                <Row>
+                    {contacts.map(({ node: contact }, index) => {
                         return (
                             <Column medium={3} key={index}>
-                                <SolutionSummary solution={solution} />
+                                <SolutionSummary solution={contact} />
                             </Column>
                         );
                     })}
