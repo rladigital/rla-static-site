a3a53b498eebccb81f6d2537595ef9f769c0379e
Chris Rodgers <chris.rodgers@rla.co.uk>
2018-04-06 16:47:28 +0100

    Adding typekit fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,6 @@
 /* globals window CustomEvent */
 import React, { createElement } from "react";
 import { Transition } from "react-transition-group";
-import WebFont from "webfontloader";
 import createHistory from "history/createBrowserHistory";
 
 import getTransitionStyle from "./src/utils/getTransitionStyle";
@@ -30,8 +29,7 @@ class ReplaceComponentRenderer extends React.Component {
         this.state = {
             exiting: false,
             nextPageResources: {},
-            scrolltop: true,
-            font: false
+            scrolltop: true
         };
         this.listenerHandler = this.listenerHandler.bind(this);
     }
@@ -56,21 +54,6 @@ class ReplaceComponentRenderer extends React.Component {
     componentDidMount() {
         window.addEventListener(historyExitingEventType, this.listenerHandler);
         window.addEventListener("scroll", () => this.handleScroll());
-
-        // Load web font
-        WebFont.load({
-            google: {
-                families: ["Montserrat:400,700,900", "sans-serif"]
-            },
-
-            active: () => {
-                this.setState({ font: "Montserrat" });
-            },
-
-            inactive: () => {
-                this.setState({ font: "Arial" });
-            }
-        });
     }
 
     componentWillUnmount() {
@@ -109,8 +92,7 @@ class ReplaceComponentRenderer extends React.Component {
                             style: getTransitionStyle({ status, timeout }),
                             nextPageResources: this.state.nextPageResources
                         },
-                        scrolltop: this.state.scrolltop,
-                        font: this.state.font
+                        scrolltop: this.state.scrolltop
                     })
                 }
             </Transition>
