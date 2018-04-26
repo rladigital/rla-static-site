d8438b052cfc4ab6bffcb57b9dfbab64989717d4
Chris Rodgers <chris.rodgers@rla.co.uk>
2018-04-26 16:57:40 +0100

    make offcanvas lines thinner

--- src/components/Offcanvas.js
+++ src/components/Offcanvas.js
@@ -303,7 +303,7 @@ class MenuIcon extends React.Component {
 
         const pathProps = {
             stroke: "currentColor",
-            strokeWidth: 6
+            strokeWidth: 5
         };
 
         const animateProps = {
