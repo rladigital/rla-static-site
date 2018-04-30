48a0f3dbbd268c6e3e6786a6ee9cb569f83fa806
Chris Rodgers <chris.rodgers@rla.co.uk>
2018-04-30 09:11:03 +0100

    scroll down padding on mobile

--- src/components/solutions/ScrollDown.js
+++ src/components/solutions/ScrollDown.js
@@ -1,19 +1,23 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
 import FAIcon from "@fortawesome/react-fontawesome";
+import { breakpoints } from "../../theme/theme";
 
 let Container = styled.div.attrs({
     role: "button"
 })`
     width: 100%;
     bottom: 0;
-    padding: 50px;
+    padding: 5px;
     position: fixed;
     text-align: center;
     transition: opacity 1s;
     cursor: pointer;
     z-index: 1;
     color: ${props => props.color};
+    @media (min-width: ${breakpoints.medium}px) {
+        padding: 50px;
+    }
 `;
 
 const fadeDown = keyframes`
