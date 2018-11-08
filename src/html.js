be0136e8b9f01385b7c5ba490303251d01ad7f28
Chris Rodgers <chris.rodgers@rla.co.uk>
2018-11-08 12:34:46 +0000

    Add Hotjar Script

--- src/html.js
+++ src/html.js
@@ -99,6 +99,25 @@ module.exports = class HTML extends React.Component {
                             `
                         }}
                     />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `(function(h,o,t,j,a,r){
+                                h.hj=h.hj||function()
+                                
+                                {(h.hj.q=h.hj.q||[]).push(arguments)}
+                                ;
+                                h._hjSettings=
+                                
+                                {hjid:1078993,hjsv:6}
+                                ;
+                                a=o.getElementsByTagName('head')[0];
+                                r=o.createElement('script');r.async=1;
+                                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                                a.appendChild(r);
+                                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                            `
+                        }}
+                    />
                 </body>
             </html>
         );
